b554fe9ca107211ae79d7573d82db39af4df7749
justjm <justhan0929@gmail.com>
2019-01-31 15:45:55 +0900

    y

--- SK_learn.py
+++ SK_learn.py
@@ -77,5 +77,5 @@ diabetes_X = diabetes.data[:,np.newaxis,2]
 diabetes_X_train = diabetes_X[:-20]
 diabetes_X_test = diabetes_X[-20:]
 
-diabetes_Y_train = diabetes_test[:-20]
-diabetes_Y_test = diabetes_test[-20:]
+diabetes_Y_train = diabetes.target[:-20]
+diabetes_Y_test = diabetes.target[-20:]
